e0972b9c43fbde2167e3c2de827f87e13cfa0458
Maximilian Pott <m.pott@suora.com>
2026-04-10 13:07:11 +0200

    Revert to Perl timestamps ($EPOCHREALTIME doesn't exist in fish)

--- shell_track.fish
+++ shell_track.fish
@@ -7,21 +7,21 @@ function __shell_track_init --on-event fish_greeting
 end
 
 function __shell_track_pre --on-event fish_preexec
-    set -g __shell_track_start $EPOCHREALTIME
+    set -g __shell_track_start (perl -MTime::HiRes=time -e 'printf "%.0f\n", time()*1000')
 end
 
 function __shell_track_post --on-event fish_postexec
     set -l exit_code $status
-    set -l end_time $EPOCHREALTIME
+    set -l end_time (perl -MTime::HiRes=time -e 'printf "%.0f\n", time()*1000')
     set -l duration 0
 
     if set -q __shell_track_start
-        set duration (math -s0 "($end_time - $__shell_track_start) * 1000")
+        set duration (math "$end_time - $__shell_track_start")
     end
 
     set -l cmd (string replace --all -- \n \\n $argv[1])
     set -l pwd_path $PWD
-    set -l timestamp (math -s0 "$end_time")
+    set -l timestamp (math -s0 "$end_time / 1000")
 
     set -l log_dir ~/.local/share/shell-track
 
